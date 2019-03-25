190bf1b24b49dc9b318e1953f02ba29a4b8299a7
hj <hujingleo01@163.com>
2019-03-25 19:23:06 +0800

    d

--- src/main/java/com/suyou/digital/StageOne.java
+++ src/main/java/com/suyou/digital/StageOne.java
@@ -17,29 +17,29 @@ public class StageOne {
      * 　　* It is observable to allow it to be watched, but only
      * 　　* reports changes when the current set is complete
      */
-    @Test
-    public void printFizzBuzz() {
-        try {
-            for (int i = 1; i <= 100; i++) {
-                if (i % 3 == 0) {
-                    if (i % 5 == 0) {
-                        System.out.println("FizzBuzz");
-                    } else {
-                        System.out.println("Fizz");
-                    }
-                } else if (i % 5 == 0) {
-                    System.out.println("Buzz");
-                } else {
-                    System.out.println(i);
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
+//    @Test
+//    public void printFizzBuzz() {
+//        try {
+//            for (int i = 1; i <= 100; i++) {
+//                if (i % 3 == 0) {
+//                    if (i % 5 == 0) {
+//                        System.out.println("FizzBuzz");
+//                    } else {
+//                        System.out.println("Fizz");
+//                    }
+//                } else if (i % 5 == 0) {
+//                    System.out.println("Buzz");
+//                } else {
+//                    System.out.println(i);
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     /**
-     * 　　* A class representing a set of packet and byte counters
+     * 　　如果是3的倍数或者包含3则是Fizz,如果是5的倍数或者包含5则是Buzz,如果既是fizz有事buzz则是FizzBuzz
      * 　　* It is observable to allow it to be watched, but only
      * 　　* reports changes when the current set is complete
      */
@@ -47,13 +47,15 @@ public class StageOne {
     public void printFizzBuzzPro() {
         try {
             for (int i = 1; i <= 100; i++) {
-                if (i % 3 == 0) {
-                    if (i % 5 == 0) {
+                String s = String.valueOf(i);
+
+                if (i % 3 == 0 || s.contains("3")) {
+                    if (i % 5 == 0 || s.contains("5")) {
                         System.out.println("FizzBuzz");
                     } else {
                         System.out.println("Fizz");
                     }
-                } else if (i % 5 == 0) {
+                } else if (i%5==0||s.contains("5")) {
                     System.out.println("Buzz");
                 } else {
                     System.out.println(i);
